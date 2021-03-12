13a949f55ff80cc4b8bcf6f37cfd286b4e5cb37f
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-03-12 19:16:58 -0300

    Conteudo inicial sobre API - aula - 01

--- src/main/java/poo2/doodle/App.java
+++ src/main/java/poo2/doodle/App.java
@@ -1,5 +1,11 @@
 package poo2.doodle;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -10,10 +16,40 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stage) {
+
+		// Teste API
+		List<String> users = consultaAPI();
+		consumeAPI(users);
+
 		this.stage = stage;
 		stage.setScene(FXMLUtil.loadScene("login"));
 		stage.setResizable(true);
 		stage.show();
+	}
+
+	private void consumeAPI(List<String> users) {
+		for (String user : users)
+			System.out.println(user);
+	}
+
+	private List<String> consultaAPI() {
+		List<String> result = new ArrayList<>();
+		try {
+			URL url = new URL("http://lucasbueno.com.br/steam.json");
+			URLConnection uc = url.openConnection();
+			InputStreamReader input = new InputStreamReader(uc.getInputStream());
+			BufferedReader in = new BufferedReader(input);
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null)
+				result.add(inputLine);
+
+			in.close();
+		} catch (Exception e) {
+			System.err.println("Erro ao comsumir API!");
+		}
+
+		return result;
 	}
 
 	static void setRoot(String fxml) {
@@ -23,7 +59,7 @@ public class App extends Application {
 	public static void main(String[] args) {
 		launch();
 	}
-	
+
 	public static void setResizable(Boolean value) {
 		stage.setResizable(value);
 	}
